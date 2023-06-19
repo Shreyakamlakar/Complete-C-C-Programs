ebc76dbd2b9546991d119872032aff65515ae9de
Shreya Patil <shreya170902@gmail.com>
2023-06-19 11:51:42 +0530

    Update StarPattern2.cpp

--- StarPattern2.cpp
+++ StarPattern2.cpp
@@ -6,7 +6,7 @@ int main()
     int n;
     printf("Enter the number of rows: ");
     scanf("%d", &n);
-        for(int i=1; i<=n; i++)
+        for(int i=n; i>=1; i--)
         {
             for(int j=1; j<=i; j++)
             {
