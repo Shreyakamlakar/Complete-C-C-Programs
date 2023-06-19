771dca25a1fffab48b63c5e0dde665564b0327bf
Shreya Patil <shreya170902@gmail.com>
2023-06-19 08:22:22 +0530

    Update Star_Pattern1.cpp

--- Star_Pattern1.cpp
+++ Star_Pattern1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                printf("* ");
+            }
+            printf("\n");
+        }
+}   
